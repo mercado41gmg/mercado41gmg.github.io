9bd2d59bc13603258df393c819284493516c0ee4
mercado41gmg <mercado41gmg@gmail.com>
2023-04-09 00:18:33 -0500

    cambio

---
+++ init.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyA7TGgZaSuVYUZndWw6UwyaAfa0qmPreCI",
+  authDomain: "mercado41gmg1.firebaseapp.com",
+  projectId: "mercado41gmg1",
+  storageBucket: "mercado41gmg1.appspot.com",
+  messagingSenderId: "953040382018",
+  appId: "1:953040382018:web:e1b867f0e59319a838a5db",
+  measurementId: "G-KL2S0NYC5K"
+     });
+ 
